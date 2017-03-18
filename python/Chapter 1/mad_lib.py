13db32f622e1632f5643eb82f3ceb826e42c1187
matryosh <tbrick1234@gmail.com>
2017-03-17 21:53:38 -0400

    Finished Chapter1: Mad Lib

---
+++ python/Chapter 1/mad_lib.py
@@ -0,0 +1,6 @@
+noun = input("Enter a noun: ")
+verb = input("Enter a verb: ")
+adjective = input("Enter a adjective: ")
+adverb = input("Enter a adverb: ")
+
+print("Do you {} your {} {} {}? That's hilarious!".format(verb, adjective, noun, adverb))
