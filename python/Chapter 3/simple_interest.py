33a85c15887baa3b34b474002e92457246ebf5f4
Trevor <tbrick1234@gmail.com>
2017-04-03 03:40:45 -0400

    Finished Chapter 3: Computing Simple Interest, in Python.

---
+++ python/Chapter 3/simple_interest.py
@@ -0,0 +1,7 @@
+principal = float(input("Enter the principal: "))
+roi = float(input("Enter the rate of interest: "))
+years = float(input("Enter number of years: "))
+
+simple_interest = principal*(1+((roi/100)*years))
+
+print("After {} years at {}%, the investment will be worth ${}.".format(years, roi, int(simple_interest)))
