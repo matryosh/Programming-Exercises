11698c39e2013ab7fe576a888fce97e48543da51
Trevor <tbrick1234@gmail.com>
2017-04-09 15:43:47 -0400

    Finished a bunch of exercises

---
+++ python/Chapter 4/legal_driving_age.py
@@ -0,0 +1,5 @@
+age = int(input("What is your age? "))
+
+old_enough = "You're not old enough to drive." if age < 16 else "You are old enough to drive."
+
+print(old_enough)
---
+++ python/Chapter 4/password_validation.py
@@ -0,0 +1,8 @@
+password = "abc$123"
+
+validation = input("What is the password? ")
+
+if validation == password:
+    print("Welcome!")
+else:
+    print("I don't know you.")
